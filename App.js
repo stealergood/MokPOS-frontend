806c2199ae3145c68fa49d7b83b9c22a43726137
Donidwimart <donidmp123@student.esaunggul.ac.id>
2024-05-13 20:47:33 +0700

    modified:   App.js 	modified:   package-lock.json 	modified:   package.json 	deleted:    src/screen/login.jsx

--- App.js
+++ App.js
@@ -5,7 +5,6 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
     </View>
   );
 }
@@ -18,3 +17,4 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
