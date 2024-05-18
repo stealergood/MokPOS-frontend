5829c6ae6a547311803a69ad1baa3b1b88198178
Hans <hanzaristo@gmail.com>
2024-05-18 11:36:35 +0700

    editing HelloScreen.js

--- src/screens/HelloScreen.js
+++ src/screens/HelloScreen.js
@@ -7,7 +7,7 @@ const HelloScreen = ({ navigation }) => {
     <View style={styles.container}>
       <Image source={require('../../assets/logo.png')} style={styles.logo} />
       <Text style={styles.title}>MokPOS</Text>
-      <Text style={styles.subtitle}>Easy Management for your Store.</Text>
+      <Text style={styles.subtitle}>Point of sales with easy management</Text>
       <Button title="Create new account" onPress={() => navigation.navigate('SignUp')} />
       <Button title="Log in" onPress={() => navigation.navigate('Login')} />
     </View>
@@ -19,16 +19,17 @@ const styles = StyleSheet.create({
     flex: 1,
     padding: 20,
     justifyContent: 'center',
-    alignItems: 'center'
   },
   logo: {
     width: 100,
     height: 100,
+    alignSelf: 'center',
     marginBottom: 20
   },
   title: {
     fontSize: 24,
-    marginBottom: 10
+    marginBottom: 10,
+    textAlign: 'center'
   },
   subtitle: {
     fontSize: 16,
